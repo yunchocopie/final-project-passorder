89e4e080d1bf5b3e055cd61495e24fa4fd53ed70
yunchocopie <yunnn687@naver.com>
2024-04-22 21:01:44 +0900

    passorder home page - list order...

---
+++ android/app/src/main/kotlin/com/example/final_project_passorder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.final_project_passorder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/holder/home/list_order/components/cafe_detail.dart
@@ -0,0 +1,24 @@
+import 'package:final_project_passorder/data/cafe.dart';
+import 'package:flutter/material.dart';
+
+class CafeDetail extends StatelessWidget {
+  final Cafe cafe;
+  const CafeDetail({super.key, required this.cafe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          cafe.cafeName,
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 4,),
+        Text(
+          cafe.like
+        )
+      ],
+    );
+  }
+}
